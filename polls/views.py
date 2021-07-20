47efe22f56413f1083e1e507d2d95aabeb29f872
hebel-dev <hebel.dev@gmail.com>
2021-07-20 17:53:14 +0200

    redairecting example, index/ and endex/

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello, world. you're at the pools")
+    return HttpResponse(
+        "hello, world. you're at the pools"
+        )
+def endex(request):
+    return HttpResponse(
+        "Its the end of the world i a now that"
+    )
 
 # Create your views here.
