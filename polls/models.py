3ff6ac1ffaa78aaf3708427ce703edd82be4cf20
hebel-dev <hebel.dev@gmail.com>
2021-07-25 15:13:09 +0200

    Changes in model like mentor , diferences between tutorial

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    create_at = models.DateTimeField('date published', auto_now_add=True)
+    updated_at = models.DateField('date updated', auto_now=True)
 
 
 class Choice(models.Model):
