4d4df20876d9af5524d66a76d69ccc13563ab4bb
hebel-dev <hebel.dev@gmail.com>
2021-07-27 09:36:25 +0200

    Add new views to admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+@admin.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'question_text',
+        'create_at',
+        'updated_at',
+
+    )
 
 # Register your models here.
